2d39705d6645de8d11e745da9dbfb42451fc7831
Anurag Gupta <anuraggupta@Anurags-MacBook-Air.local>
2025-02-13 11:09:41 +0530

    Second Episode 3 first part

--- App.js
+++ App.js
@@ -1,19 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// const heading = React.createElement("h1", {id:"heading"}, "Hello world from React");
-// console.log(heading); // returns object
+//react.createelement => reactelemnt-js object => htmlelement(render);
 
-const parent = React.createElement("div", {id: "parent"}, [
-    React.createElement("div", {id: "child1"},[
-        React.createElement("h1", {}, "This is namaste react"),
-        React.createElement("h2", {}, "I am heading 2")
-    ]),
-    React.createElement("div", {id: "child1"},[
-        React.createElement("h1", {}, "I am heading 1"),
-        React.createElement("h2", {}, "I am heading 2")
-    ])
-])
+// React createelement => object => html(render);
+
+// const heading  = React.createElement("h1",{id:"heading"},"This is heading with react element");
+
+//using JSX;
+//JSX(transpiled before it reaches the JS engine) JSX got tranpiled to JS by parcel using babel package
+//JSX => react.createElement => reactElement-js object => html element(render)
+
+//react element
+const jsxheading = (
+  <h1 id="heading" tabIndex={5}>
+    Namaste React using JSX
+  </h1>
+);
+
+//React Component
+//class-based component - old
+//functiocal component - new
+
+//React functional compponent
+const headingComponent = () => {
+  return <h1 className="heading">React functional component</h1>;
+};
+//or
+const headingComponent2 = () => (
+  <h1 className="heading">Namaste React with Anurag</h1>
+); //better syntax using arrow function
+
+// above both function are same
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+// root.render(jsxheading); //rendering react element
+
+root.render(headingComponent()); //rendering react component
