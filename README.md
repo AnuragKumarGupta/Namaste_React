14abfc20b623da19b8e2ea10a48228367ba17969
Anurag Gupta <anuraggupta@Anurags-MacBook-Air.local>
2025-04-04 08:05:09 +0530

    episode 6 namaste react

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Namaste_React
 
 # Parcel
+
 - Dev Build
 - Local server
--  HMR- hot module replacement //auto update the specific module after any changes made
+- HMR- hot module replacement //auto update the specific module after any changes made
 - file watching algorithm which make HMR-- written in c++.
 - Caching- Build faster
 - Image optimization
@@ -11,10 +12,39 @@
 - Bundling
 - compressing
 - consistent hashinh
--  code splitting
+- code splitting
 - Differential bundling- to support older browser
 - Diagnostic
 - Error Handling
 - HTTPS
 - Tree shaking // remove unused code
-- Different  dev and prod build
+- Different dev and prod build
+
+# Namaste food app
+
+/_
+Header
+-logo
+-nav item
+Body
+-search
+-restaurant container
+-restaurant card
+Footer
+-copyright
+-links
+-Address
+-contacts info
+_/
+
+two types of export/import
+
+-default export/import
+
+export default component;
+import component from path;
+
+-named export/import
+
+export const component;
+import {component} from path
---
+++ src/components/Body.js
@@ -0,0 +1,77 @@
+import RestaurantCard from "./RestaurantCard";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
+const Body = () => {
+  //local store variable-- super power variable
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  // Whenever state variables update, react triggers a reconciliation cycle(re-renders the component)
+  //local state variable
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
+    //optional chaining
+    setListOfRestaurants(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredRestaurant(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  return listOfRestaurants?.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="body">
+      <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              const filteredRestaurant = listOfRestaurants.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setFilteredRestaurant(filteredRestaurant);
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {filteredRestaurant?.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
